83fc31f1c24b163a4e932d24518f94ae98b2b701
dollz <128159325+Dollzzy@users.noreply.github.com>
2023-04-15 02:10:30 +0300

    1.3

--- Version.js
+++ Version.js
@@ -1,10 +1,38 @@
 let versiune = document.querySelector(".Verion")
 let NewsV = document.querySelector(".NewsV")
-let v = '1.2-Alpha'
+let v = '1.3-Alpha - 15 apr 2023'
 let n = `
     -Modificare design<br>
-    -Adaugare pagina de info<br>
-`
+    -Modificare Pagina versiune<br>
+    <br><br><br>Info: De retinut site-ul va dura ceva pana va putea fi functional
+     deoarece eu voi fi nevoit sa modific jumate din tot codul HTML,css,Javascript <br><br>
+     De ce dureaza atat sa termin site-ul?
+     <br><br>
+     Eu in curand voi da un test de trecere si trebuie sa invat si nu prea mai am timp sa lucrez la acest site
+     dar usor cu usor incerc sal gat pana in 1 Mai .<br>
+     <br><br>
+     Nu poti sa gati site-ul mai devreme?
+     <br><br>
+     O sa incerc sal gat cat mai curand posibil finca in fiecare weekend voi lucra la site.
+     <br><br>
+     Ce a de gand sa fac?
+     <br><br>
+     Ei bine mam gandi sa refac tot site-ul de la 0 si sal fac conform design-ului 0.2.0
+     <br><br>
+     Chiar o sa fac asta?
+     <br><br>
+     Cred ca da nus sigur dar asa ar fi cel mai rapid mod de a gata acest update fara a ma uita prin 700+ lini de cod css
+     <br><br>
+     Concluzie:
+     <br><br>
+     De maine ma voi pune sa refac tot site-ul conform design-ului 0.2.0
+     <br>Site-ul acesta va ramane disponibi.
+     <br><br>
+     Cand va fi disponibil site-ul ?
+     <br><br>
+     In 20 de zile sau chiar mai putin site-ul va fi gata
+     <br><br><br><br>final
+     `
 
     versiune.innerHTML ='Versiune: ' + v
-    NewsV.innerHTML = 'Ce e nou?<br>' + n
+    NewsV.innerHTML = 'Ce e nou?<br><br>' + n
